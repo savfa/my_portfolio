6de47d00a4f3cedcba85ebd96af8734535147ab5
savfa <savfa@yandex.ru>
2024-04-06 00:48:33 +0300

    work on main section

---
+++ src/index.ts
@@ -0,0 +1,2 @@
+import "./common/styles/styles.scss";
+// import "./common/assets/images/hero.jpg";
--- vite.config.ts
+++ vite.config.ts
@@ -1,57 +1,61 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { defineConfig, loadEnv } from "vite";
-import pugPlugin from "vite-plugin-pug"
+// eslint-disable-next-line import/no-extraneous-dependencies
+import pugPlugin from "vite-plugin-pug";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+// eslint-disable-next-line import/no-extraneous-dependencies
 import { resolve } from "path";
 
 // https://vitejs.dev/config/
 
-const options: any = { pretty: true } // FIXME: pug pretty is deprecated!
-const locals = { name: "My Pug" }
+const options: any = { pretty: true }; // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" };
 
 export default defineConfig(({ command, mode }) => {
   // const env = loadEnv(mode, process.cwd(), "");
 
-  const root = resolve(__dirname, './src/core/pages');
-
+  const root = resolve(__dirname, "./src");
 
   return {
-    plugins: [pugPlugin(options, locals)],
+    plugins: [pugPlugin(options, locals), ViteImageOptimizer()],
     root,
-    /*resolve: {
+    /* resolve: {
       alias: {
         src: "src/",
         test: resolve(__dirname, "/core/pages/")
       },
-    },*/
+    }, */
+    publicDir: "common/assets",
     build: {
-      outDir: "../../../build",
-      publicDir: "src/common/public",
+      outDir: "../build",
       rollupOptions: {
         input: {
-          main: resolve(root, 'index.html'),
-          test: resolve(root, 'test/index.html')
+          main: resolve(root, "index.html"),
         },
-        /*output: { // remove hashes from filenames on build
+        /* output: { // remove hashes from filenames on build
           entryFileNames: `assets/[name].js`,
           chunkFileNames: `assets/[name].js`,
           assetFileNames: `assets/[name].[ext]`,
-        },*/
-        output: { // remove hashes from filenames on build
-          assetFileNames: ({name}) => {
-            name = name.toLowerCase();
+        }, */
+        output: {
+          // remove hashes from filenames on build
+          assetFileNames: ({ name }) => {
+            const prepareName = name.toLowerCase();
 
             if (/\.(gif|jpe?g|png|svg)$/.test(name ?? ``)) {
-              return `assets/images/[name]-[hash][extname]`
+              return `assets/images/[name][extname]`;
             }
 
-            if (/\.css$/.test(name ?? ``)) {
-              return `assets/styles/[name]-[hash][extname]`
+            if (/\.css$/.test(prepareName ?? ``)) {
+              return `assets/styles/[name]-[hash][extname]`;
             }
 
             // default
-            return `assets/[name]-[hash][extname]`
+            return `assets/[name]-[hash][extname]`;
           },
-        }
-      }
+        },
+      },
     },
   };
 });
