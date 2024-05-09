2b397a6ef7a75ca76ae21498113defefd7a7081e
savfa <savfa@yandex.ru>
2024-05-09 05:00:44 +0300

    work on about section

--- src/core/_components/buttons/buttons.ts
+++ src/core/_components/buttons/buttons.ts
@@ -1,8 +1,16 @@
 const downloadResume = () => {
-  window.alert("Скачать резюме");
+  window.open(
+    "https://voronezh.hh.ru/applicant/resumes/view?resume=8b9283fdff0210651d0039ed1f777a4149497a",
+    "_blank"
+  );
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  const downloadResumeButton = document.getElementById("downloadResume");
-  downloadResumeButton?.addEventListener("click", downloadResume);
+  const downloadResumeButtons = document.querySelectorAll(
+    '[data-id="downloadResume"]'
+  );
+
+  downloadResumeButtons.forEach((button) => {
+    button.addEventListener("click", downloadResume);
+  });
 });
